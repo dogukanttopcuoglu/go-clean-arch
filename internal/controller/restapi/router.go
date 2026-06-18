4798b73fc23ac3a0423f10a33971904e4869d0fd
Doğukan Topçuoğlu <155321205+dogukanttopcuoglu@users.noreply.github.com>
2026-06-18 14:52:14 +0300

    feat: add zap logger

--- internal/controller/restapi/router.go
+++ internal/controller/restapi/router.go
@@ -5,16 +5,17 @@ import (
 
 	v1 "github.com/dogukanttopcuoglu/clean-lab/internal/controller/restapi/v1"
 	"github.com/dogukanttopcuoglu/clean-lab/internal/usecase"
+	"github.com/dogukanttopcuoglu/clean-lab/pkg/logger"
 	"github.com/gofiber/fiber/v2"
 )
 
-func NewRouter(app *fiber.App, userUseCase usecase.User, taskUseCase usecase.Task) {
+func NewRouter(app *fiber.App, userUseCase usecase.User, taskUseCase usecase.Task, log logger.Logger) {
 	app.Get("healthz", func(ctx *fiber.Ctx) error {
 		return ctx.SendStatus(http.StatusOK)
 	})
 
 	apiV1Group := app.Group("v1")
 	{
-		v1.NewRoutes(apiV1Group, userUseCase, taskUseCase)
+		v1.NewRoutes(apiV1Group, userUseCase, taskUseCase, log)
 	}
 }
--- internal/controller/restapi/v1/controller.go
+++ internal/controller/restapi/v1/controller.go
@@ -1,8 +1,12 @@
 package v1
 
-import "github.com/dogukanttopcuoglu/clean-lab/internal/usecase"
+import (
+	"github.com/dogukanttopcuoglu/clean-lab/internal/usecase"
+	"github.com/dogukanttopcuoglu/clean-lab/pkg/logger"
+)
 
 type V1 struct {
 	taskUseCase usecase.Task
 	userUseCase usecase.User
+	log         logger.Logger
 }
